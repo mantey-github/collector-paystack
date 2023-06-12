128c17199cb33003ca405d2d1a963af003fa9fee
lacasera <aboateng62@gmail.com>
2023-06-12 09:28:03 +0000

    clean code

--- src/Http/Controllers/BillingPortalController.php
+++ src/Http/Controllers/BillingPortalController.php
@@ -10,25 +10,12 @@ class BillingPortalController
 {
     public function __invoke(Request $request)
     {
-        // $type = $type ?: $this->guessBillableType();
-
-        //  $billable = $this->billable($type, $id);
-
         Inertia::setRootView('collector::app');
 
         View::share([
             'cssPath' => __DIR__.'/../../../public/css/app.css',
             'jsPath' => __DIR__.'/../../../public/js/app.js',
-            //   'translations' => static::getTranslations(),
         ]);
-
-        //        if ($request->checkout === 'subscription_started') {
-        //            session(['spark.flash.success' => __('Your subscription has been started successfully.')]);
-        //        } elseif ($request->checkout === 'payment_method_added') {
-        //            session(['spark.flash.success' => __('Your payment method has been added successfully.')]);
-        //        }
-
-        // Inertia::share(app(FrontendState::class)->current($type, $billable));
 
         return Inertia::render('BillingPortal');
     }
